a0b08055e3e73af8865b447b3c58236ec3829343
matthew-shtyasek <shtyasek2003@mail.ru>
2021-11-14 13:05:26 +0500

    Adding course-work (Breaking the Caesar cipher)

---
+++ Course/AlphaDialog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Course {
+    public partial class inputAlph : Form {
+        public inputAlph() {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            button1.Enabled = Convert.ToBoolean(textBox1.Text.Length);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            ceasarForm.alpha = textBox1.Text.ToLower();
+            ceasarForm.dictId = -1;
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            Close();
+        }
+    }
+}
